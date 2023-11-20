c5e49345a92826be5a37798de2ba74479fa7cf48
lpeese <lopicpeese@email.com>
2023-11-21 00:28:03 +0800

    test

--- vyos/build.sh
+++ vyos/build.sh
@@ -7,4 +7,4 @@ apt install -y packer qemu-system-x86 qemu-utils
 rm -rf /tmp/output-vyos-rolling-amd64
 
 VYOSURL=$(curl -skL https://vyos.net/get/nightly-builds | awk -F'"' '/amd64.iso/ {print $2;exit}')
-sed -e "s/XURLX/${VYOSURL}/" vyos/vyos.json | packer build -
+sed -e "s|XURLX|${VYOSURL}|" vyos/vyos.json | packer build -
