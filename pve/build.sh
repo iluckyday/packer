5a6af9036822d05823e9493d6ab428ec3791adee
iluckyday <luckyday1990@mail.com>
2024-01-06 16:08:52 +0800

    test

--- pve/build.sh
+++ pve/build.sh
@@ -5,6 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y packer qemu-system-x86 qemu-utils sshpass guestfs-tools
 
+packer plugins install github.com/hashicorp/qemu
+
 PVEVER=$(curl -skL http://download.proxmox.com/iso/SHA256SUMS | awk '{if($2~/^proxmox-ve/) print $2}' | awk 'END {sub(/proxmox-ve_/,"",$1);sub(/.iso/,"",$1);print $1}')
 PVEURL="http://download.proxmox.com/iso/proxmox-ve_"${PVEVER}".iso"
 
